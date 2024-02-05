4f09863a1bddddb2fe224e31d64611cf1cdcb058
sakthiofficial <sakthivel.g@alliancezone.in>
2024-02-05 13:37:17 +0530

    mail turn on

--- src/helper/email/mailOptions.js
+++ src/helper/email/mailOptions.js
@@ -7,7 +7,7 @@ export function superAdminMailOptions(
 ) {
   return {
     from: "CP PORTAL HYDERABAD <sakthivel.g@alliancezone.in>",
-    to: "sakthiroky123@gmail.com",
+    to: "sakthiroky123@gamil.com",
     subject: "New User Created in CP Portal",
     html: `
         <html>
